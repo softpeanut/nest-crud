70ceb6845caf7301bfb617e0643cd85314e50806
김범진 <sdpthf@naver.com>
2021-12-31 08:59:23 +0900

    ⚡ :: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
+import 'dotenv/config'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 32771,
-      username: 'banzi',
-      password: '12341234',
-      database: 'banzi',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
       synchronize: true,
     }),
     UserModule,
