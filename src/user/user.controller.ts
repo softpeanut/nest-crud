578ef69af637360056e628afdae832a7d73343f1
김범진 <sdpthf@naver.com>
2021-12-31 12:05:49 +0900

    ⚡ :: update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { PostDto } from './dto/post.dto';
+import { UpdateDto } from './dto/update.dto';
 
 @Controller('user')
 export class UserController {
@@ -16,4 +17,15 @@ export class UserController {
     postUser(@Body() dto: PostDto): Promise<string> {
         return this.userService.postUser(dto);
     }
+
+    @Patch(':id')
+    updateUserWithPathParam(@Param('id') userId: number, @Body() dto: UpdateDto): Promise<string> {
+        return this.userService.updateUser(userId, dto);
+    }
+
+    @Patch()
+    updateUserWithQueryString(@Query('userId') userId: number, @Body() dto: UpdateDto): Promise<string> {
+        return this.userService.updateUser(userId, dto);
+    }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostDto } from './dto/post.dto';
+import { UpdateDto } from './dto/update.dto';
 
 import { User } from './user.entity';
 
@@ -19,7 +20,7 @@ export class UserService {
     async postUser(dto: PostDto): Promise<string> {
 
         const user: User = new User();
-        
+
         user.userId = dto.userId;
         user.password = dto.password;
         user.name = dto.name;
@@ -28,5 +29,17 @@ export class UserService {
 
         await this.userRepository.save(user);
         return "success post";
+    }
+
+    async updateUser(userId: number, dto: UpdateDto): Promise<string> {
+
+        const user: User = await this.userRepository.findOne(userId);
+
+        user.name = dto.name;
+        user.phone = dto.phone;
+        user.email = dto.email;
+
+        await this.userRepository.save(user);
+        return "success update";
     }
 }
